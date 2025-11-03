ed57070313c32d009825123f64da29e6038fb81a
Vova <103131693+vovkasWeb@users.noreply.github.com>
2025-11-04 00:54:04 +0200

    .

---
+++ js/obj.js
@@ -0,0 +1,181 @@
+export const globalGames = [
+  {
+    name: 'Bee Keeper',
+    provider: 'Pragmatic Play',
+    img: {
+      url: './resurs/game/Bee Keeper.jpg',
+      alt: 'Bee Keeper Slot by Pragmatic Play'
+    },
+    urlPage: './free-pokies/bee-keeper',
+  },
+  {
+    name: 'Mystic Wishes',
+    provider: 'Pragmatic Play',
+    img: {
+      url: './resurs/game/Mystic Wishes.jpg',
+      alt: 'Mystic Wishes Slot by Pragmatic Play'
+    },
+    urlPage: './free-pokies/mystic-wishes',
+  },
+  {
+    name: 'Sweet Rush Bonanza',
+    provider: 'Pragmatic Play',
+    img: {
+      url: './resurs/game/Sweet Rush Bonanza.jpg',
+      alt: 'Sweet Rush Bonanza Slot by Pragmatic Play'
+    },
+    urlPage: './free-pokies/sweet-rush-bonanza',
+  },
+  {
+    name: '3 Royal Coins',
+    provider: 'Playson',
+    img: {
+      url: './resurs/game/3 Royal Coins.jpg',
+      alt: '3 Royal Coins Slot by Playson'
+    },
+    urlPage: './free-pokies/3-royal-coins',
+  },
+  {
+    name: 'Fortune Spin',
+    provider: 'Bgaming',
+    img: {
+      url: './resurs/game/Fortune Spin.jpg',
+      alt: 'Fortune Spin Slot by Bgaming'
+    },
+    urlPage: './free-pokies/fortune-spin',
+  },
+  {
+    name: 'Big Red',
+    provider: 'Aristocrat',
+    img: {
+      url: './resurs/game/Big Red.jpg',
+      alt: 'Big Red Slot by Aristocrat'
+    },
+    urlPage: './free-pokies/big-red',
+  },
+  {
+    name: 'Gates of Olympus',
+    provider: 'Pragmatic Play',
+    img: {
+      url: './resurs/game/Gates of Olympus.jpg',
+      alt: 'Gates of Olympus Slot by Pragmatic Play'
+    },
+    urlPage: './free-pokies/gates-of-olympus',
+  },
+  {
+    name: 'Wolf Gold',
+    provider: 'Pragmatic Play',
+    img: {
+      url: './resurs/game/Wolf Gold.jpg',
+      alt: 'Wolf Gold Slot by Pragmatic Play'
+    },
+    urlPage: './free-pokies/wolf-gold',
+  },
+  {
+    name: 'Chicken Road 2',
+    provider: 'InOut',
+    img: {
+      url: './resurs/game/Chicken Road 2.jpg',
+      alt: 'Chicken Road 2 Crash Slot by InOut'
+    },
+    urlPage: './free-pokies/chicken-road-2',
+  },
+  {
+    name: 'Panda Magic',
+    provider: 'Aristocrat',
+    img: {
+      url: './resurs/game/Panda Magic.jpg',
+      alt: 'Panda Magic Slot by Aristocrat'
+    },
+    urlPage: './free-pokies/panda-magic',
+  },
+  {
+    name: 'Big Ben',
+    provider: 'Aristocrat',
+    img: {
+      url: './resurs/game/Big Ben.jpg',
+      alt: 'Big Ben Slot by Aristocrat'
+    },
+    urlPage: './free-pokies/big-ben',
+  },
+  {
+    name: 'Autumn Moon',
+    provider: 'Aristocrat',
+    img: {
+      url: './resurs/game/Autumn Moon.jpg',
+      alt: 'Autumn Moon Slot by Aristocrat'
+    },
+    urlPage: './free-pokies/autumn-moon',
+  },
+  {
+    name: 'Tiki Fire',
+    provider: 'Aristocrat',
+    img: {
+      url: './resurs/game/Tiki Fire.jpg',
+      alt: 'Tiki Fire Slot by Aristocrat'
+    },
+    urlPage: './free-pokies/tiki-fire',
+  },
+  {
+    name: 'Golden Century',
+    provider: 'Aristocrat',
+    img: {
+      url: './resurs/game/Golden Century.jpg',
+      alt: 'Golden Century Slot by Aristocrat'
+    },
+    urlPage: './free-pokies/golden-century',
+  },
+  {
+    name: 'Chicken Road',
+    provider: 'InOut',
+    img: {
+      url: './resurs/game/Chicken Road.jpg',
+      alt: 'Chicken Road Crash Slot by InOut'
+    },
+    urlPage: './free-pokies/chicken-road',
+  },
+  {
+    name: 'Avia Masters',
+    provider: 'Bgaming',
+    img: {
+      url: './resurs/game/Avia Masters.jpg',
+      alt: 'Avia Masters Crash Slot by Bgaming'
+    },
+    urlPage: './free-pokies/avia-masters',
+  },
+  {
+    name: 'Aviatrix',
+    provider: 'Aviatrix',
+    img: {
+      url: './resurs/game/Aviatrix.jpg',
+      alt: 'Aviatrix Crash Slot'
+    },
+    urlPage: './free-pokies/aviatrix',
+  },
+  {
+    name: 'Space XY',
+    provider: 'Bgaming',
+    img: {
+      url: './resurs/game/Space XY.jpg',
+      alt: 'Space XY Crash Slot by Bgaming'
+    },
+    urlPage: './free-pokies/space-xy',
+  },
+  {
+    name: 'Christmas Crash',
+    provider: 'Evoplay',
+    img: {
+      url: './resurs/game/Christmas Crash.jpg',
+      alt: 'Christmas Crash Slot by Evoplay'
+    },
+    urlPage: './free-pokies/christmas-crash',
+  },
+];
+
+export const sliderGamesMain = {
+   'new':[globalGames[0],globalGames[1],globalGames[2],globalGames[3],globalGames[4],globalGames[8],globalGames[9]],
+   'trending':[globalGames[5],globalGames[6],globalGames[7],globalGames[8],globalGames[9]],
+   'aristocrat':[globalGames[10],globalGames[11],globalGames[12],globalGames[9],globalGames[13]],
+   'crash':[globalGames[14],globalGames[15],globalGames[16],globalGames[17],globalGames[18]]
+}
+
